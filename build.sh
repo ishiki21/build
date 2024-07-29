b5cb64a661a381414529fa0232a64d935e77dfa1
ishiki21 <paleloebarbershop@gmail.com>
2024-07-30 00:34:39 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "Cloning device stuffs"
 
-git clone --depth=1 git@github.com:ishiki21/vxm.git -b lineage-20.0 vendor/xiaomi/mojito
+git clone --depth=1 https://github.com/ishiki21/vxm.git vendor/xiaomi/mojito
 
-git clone --depth=1 git@github.com:ishiki21/dxm.git -b ris-14 device/xiaomi/mojito
+git clone --depth=1 https://github.com/ishiki21/dxm.git device/xiaomi/mojito
 
-git clone --depth=1 git@github.com:ishiki21/kxm.git -b kernel/xiaomi/mojito
+git clone --depth=1 https://github.com/ishiki21/vxm.git kernel/xiaomi/mojito
